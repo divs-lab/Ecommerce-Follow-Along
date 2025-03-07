dc1a28f5b02511897ed574d2aefe428388e44c8b
Divya Singh <divyasingh5028@gmail.com>
2025-03-07 12:29:16 +0530

    Mileston24

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,12 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import { Login } from './components/login'
 import { Signup } from './components/signup'
 import { Home } from './page/Home'
-import {Productform} from './components/productform'
+import {ProductForm, Productform} from './components/productform'
 import { Productcardseller } from './components/productcardforseller'
 import {SingleCard} from './components/singlecard'
 import {CreateAddress} from './components/address';
 import Cart from './page/cart'
 import SelectAddress from './page/selectAddress'
+import OrderConfirmation from './page/orderConfirmation'
 
 
 function App() {
@@ -19,12 +20,13 @@ function App() {
       <Route path="/" element={<Home/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/signup" element={<Signup/>} />
-      <Route path="/page/cart" element={<Cart/>}/>
-      <Route path="/productform" element={<Productform/>} />
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/productform" element={<ProductForm/>} />
       <Route path="/my-product" element={<Productcardseller/>}/>
       <Route path="/products/:id" element={<SingleCard/>}/>
       <Route path="/address" element={<CreateAddress/>}/>
-      <Route path="/page/selectAddress" element={<SelectAddress/>}/>
+      <Route path="/selectAddress" element={<SelectAddress/>}/>
+      <Route path="/order-confirm" element={OrderConfirmation}/>
     </Routes>
     </>
   )
